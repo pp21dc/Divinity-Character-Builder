804c5cad309ffcf0c436fa070733ec060d1ca53e
Patrick Perry <patrickperry713@gmail.com>
2023-03-05 15:01:30 -0500

    js scripts to add elements to table

---
+++ build-table.js
@@ -0,0 +1,35 @@
+let Aerotheurge = [
+    {Name: "Blinding Radiance", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "1", AP_Cost: "2", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Electric Discharge", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "1", AP_Cost: "2", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Favorable Wind", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "1", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Shocking Touch", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "1", AP_Cost: "2", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Teleportation", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "4", AP_Cost: "2", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Closed Circuit", P_Aero: "3", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "13", AP_Cost: "2", SP_Cost: "2", Mem_Required: "2"},
+    {Name: "Chain Lightning", P_Aero: "3", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "9", AP_Cost: "2", SP_Cost: "", Mem_Required: "2"},
+    {Name: "Nether Swap", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "9", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Dazing Bolt", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "4", AP_Cost: "3", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Vacuum Aura", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "2", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "C", AP_Cost: "2", SP_Cost: "1", Mem_Required: "2"},
+    {Name: "Vacuum Touch", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "1", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "C", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Breathing Bubble", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "1", Lvl_Avalible: "C", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Mass Breathing Bubbles", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "2", Lvl_Avalible: "C", AP_Cost: "1", SP_Cost: "1", Mem_Required: "2"},
+    {Name: "Evasive Aura", P_Aero: "2", P_Geo: "", P_Hun: "2", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "C", AP_Cost: "2", SP_Cost: "1", Mem_Required: "2"},
+    {Name: "Uncanny Evasion", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "4", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Blessed Smoke Cloud", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "2", P_Sum: "", P_War: "", Lvl_Avalible: "C", AP_Cost: "1", SP_Cost: "2", Mem_Required: "2"},
+    {Name: "Smoke Cover", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "1", P_Sum: "", P_War: "", Lvl_Avalible: "C", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Thunderstorm", P_Aero: "5", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "16", AP_Cost: "4", SP_Cost: "3", Mem_Required: "3"},
+    {Name: "Apportation", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "9", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Superconductor", P_Aero: "3", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "16", AP_Cost: "3", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Pressure Spike", P_Aero: "2", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "9", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Erratic Wisp", P_Aero: "1", P_Geo: "", P_Hun: "1", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "C", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Tornado", P_Aero: "3", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "16", AP_Cost: "2", SP_Cost: "", Mem_Required: "1"},
+    {Name: "Vaporise", P_Aero: "1", P_Geo: "", P_Hun: "", P_Hyd: "", P_Nec: "", P_Pol: "1", P_Pyr: "", P_Sco: "", P_Sum: "", P_War: "", Lvl_Avalible: "C", AP_Cost: "1", SP_Cost: "", Mem_Required: "1"},
+]
+
+function createRow(){
+    var table = document.getElementById("html-data-table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "new cell1";
+    cell2.innerHTML = "new cell2";
+}
---
+++ spell.js
@@ -0,0 +1,20 @@
+class Spell{
+    constructor(Name, p_Aer, p_Geo, p_Hun, p_Hyd, p_Nec, p_Pol, p_Pyr, p_Sco, p_Sum, p_War, MemCost, APCost, SPCost, inList){
+
+        this.Name = Name;
+        this.p_Aer = p_Aer;
+        this.p_Geo = p_Geo;
+        this.p_Hun = p_Hun;
+        this.p_Hyd = p_Hyd;
+        this.p_Nec = p_Nec;
+        this.p_Pol = p_Pol;
+        this.p_Pyr = p_Pyr;
+        this.p_Sco = p_Sco;
+        this.p_Sum = p_Sum;
+        this.p_War = p_War;
+        this.MemCost = MemCost;
+        this.APCost = APCost;
+        this.SPCost = SPCost;
+        this.inList = inList;
+    }
+}
